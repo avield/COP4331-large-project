6651102ef6583d9a4dbce4a2d13cb10b1e890b9c
luciano093 <luparedes100@gmail.com>
2026-03-16 21:28:39 -0400

    Change project to darkmode

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -36,7 +36,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
+    <html lang="en" className="dark">
       <head>
         <HeadContent />
       </head>
