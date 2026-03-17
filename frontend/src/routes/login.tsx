228c4b0a0942b2523346a7ed66305199f1609e5d
luciano093 <luparedes100@gmail.com>
2026-03-16 21:44:48 -0400

    Add login page

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,14 +1,69 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+
 export const Route = createFileRoute('/login')({
   component: Login,
 })
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login to your account</h1>
-      <form>
-      </form>
+    <div className="flex items-center justify-center w-screen h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className='text-center'>Log In</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="user@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  placeholder="********"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Register
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
