02eeafa18fbfc5b391b3e2ce6877508acba503f5
luciano093 <luparedes100@gmail.com>
2026-03-17 20:38:41 -0400

    Add home starting screen

--- frontend/src/routes/_workspace/home.tsx
+++ frontend/src/routes/_workspace/home.tsx
@@ -1,9 +1,25 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
+import { FolderPlus, Plus } from 'lucide-react'
 
 export const Route = createFileRoute('/_workspace/home')({
   component: Home,
 })
 
 function Home() {
-  return <div>Hello "/(workspace)/home"!</div>
+  return (
+    <div className="flex h-[80vh] flex-col items-center justify-center gap-4 text-center">
+      <div className="flex size-20 items-center justify-center rounded-full bg-muted">
+        <FolderPlus className="size-10 text-muted-foreground" />
+      </div>
+      <h2 className="text-2xl font-bold tracking-tight">Welcome to Taskademia!</h2>
+      <p className="text-muted-foreground max-w-sm">
+        You don't have any active projects yet. Create your first workspace to start managing your tasks.
+      </p>
+      <Button className="mt-4 cursor-pointer gap-1">
+        <Plus /> 
+        Create First Project
+      </Button>
+    </div>
+  )
 }
