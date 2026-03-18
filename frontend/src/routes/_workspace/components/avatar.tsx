a990e316d6c9e05e3561ce8e080b6bf068b1d387
luciano093 <luparedes100@gmail.com>
2026-03-17 21:16:52 -0400

    Make avatar dropdown feel more responsive

--- frontend/src/routes/_workspace/components/avatar.tsx
+++ frontend/src/routes/_workspace/components/avatar.tsx
@@ -6,20 +6,20 @@ export default function NavbarAvatar() {
   return <>
     <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full cursor-pointer">
+            <Button variant="ghost" size="icon" className="rounded-full cursor-pointer active:scale-100 active:translate-y-0">
                 <Avatar size="lg">
                     <AvatarFallback>User</AvatarFallback>
                 </Avatar>
             </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-32">
+        <DropdownMenuContent className="w-32 duration-10 mt-1 mr-1">
             <DropdownMenuGroup>
-            <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">Settings</DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onSelect={(e) => e.preventDefault()}>Profile</DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onSelect={(e) => e.preventDefault()}>Settings</DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-            <DropdownMenuItem className="cursor-pointer" variant="destructive">Log out</DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" variant="destructive" onSelect={(e) => e.preventDefault()}>Log out</DropdownMenuItem>
             </DropdownMenuGroup>
         </DropdownMenuContent>
     </DropdownMenu>
