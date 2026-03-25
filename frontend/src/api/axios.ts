00235a593946c63a69942bba1cf722a64b337a1e
luciano093 <luparedes100@gmail.com>
2026-03-25 11:33:06 -0400

    Add frontend request/response middleware

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useAuthStore } from './authStore';
+
+const api = axios.create({
+    baseURL: `${import.meta.env.BACKEND_URL}`,
+    withCredentials: true
+});
+
+api.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().accessToken;
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response, // If we don't get any errors, we return the response as is
+    async (error) => {
+        const originalRequest = error.config;
+
+        // We use _retry in here to not retry if we get an error even after the refresh token was successfully refreshed
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const response = await axios.post(`${import.meta.env.BACKEND_URL}/refresh`, {}, { withCredentials: true });
+
+                useAuthStore.getState().setAccessToken(response.data.accessToken);
+
+                return api(originalRequest);
+            } catch (refreshError) {
+                // The refresh token likely expired too, so we log the user out
+                useAuthStore.getState().clearAuth();
+
+                return Promise.reject(refreshError);
+            }
+        }
+        
+        return Promise.reject(error);
+    }
+)
+
+export default api;
