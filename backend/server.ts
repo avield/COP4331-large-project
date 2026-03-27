dd420f7aa52c94221af61401fab4d5013f45efe4
Aviel Dahaman <avield@gmail.com>
2026-03-27 19:30:35 -0400

    Add search end point

--- backend/server.ts
+++ backend/server.ts
@@ -8,6 +8,7 @@ import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import projectMemberRoutes from './routes/projectMemberRoutes.js';
 import profileRoute from './routes/profileRoutes.js';
+import searchRoute from './routes/searchRoute.js';
 import rateLimit from 'express-rate-limit';
 import { styleText } from "node:util";
 import { exit } from 'node:process';
@@ -65,6 +66,7 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use('/api/project-members', projectMemberRoutes);
 app.use('/api/users', profileRoute);
+app.use("api/search", searchRoute);
 
 const PORT: number = Number(process.env.PORT) || 5000;
 
