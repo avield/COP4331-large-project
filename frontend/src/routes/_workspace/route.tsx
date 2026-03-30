c141607b2f5a4eccbe0db6d40f0833e34c4ab1b4
Manny <manny@Mac.lan>
2026-03-30 08:42:44 -0400

    feat: add alert-dialog component, delete project button

--- frontend/src/routes/_workspace/route.tsx
+++ frontend/src/routes/_workspace/route.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import UiSidebar from './components/sidebar'
 import Navbar from './components/navbar'
@@ -8,19 +8,17 @@ export const Route = createFileRoute('/_workspace')({
 })
 
 function RouteComponent() {
-  return <>
-    <SidebarProvider>
-      <UiSidebar />
-      <main className="flex-1 w-full flex flex-col">
-        <Navbar />
-        <div className="p-2 md:hidden">
-          <SidebarTrigger variant="ghost" />
-        </div>
-
-        <div className="p-4">
-          <Outlet />
-        </div>
-      </main>
-    </SidebarProvider>
-  </>
+  return (
+    <div className="dark min-h-screen bg-background">
+      <SidebarProvider>
+        <UiSidebar />
+        <main className="flex-1 w-full flex flex-col">
+          <Navbar />
+          <div className="p-4 md:p-6">
+            <Outlet />
+          </div>
+        </main>
+      </SidebarProvider>
+    </div>
+  )
 }
