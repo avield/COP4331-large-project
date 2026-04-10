e79c2acdd2fb59add1bfb9ce181af57aa3ca6c9b
Julio Maldonado <leocrow32809@yahoo.com>
2026-04-10 00:03:21 -0400

    Fix small bug with redirect to a user's Project.

--- frontend/src/routes/_workspace/users.$userId.tsx
+++ frontend/src/routes/_workspace/users.$userId.tsx
@@ -116,7 +116,7 @@ function UserProfilePage() {
                                 projects.map((project: Project) => (
                                     <Link
                                         key={project._id}
-                                        to="/_workspace/projects/$projectId"
+                                        to="/projects/$projectId"
                                         params={{ projectId: project._id }}
                                         className="block p-5 border rounded-xl hover:shadow-md hover:border-primary/50 transition-all bg-card text-card-foreground group"
                                     >
